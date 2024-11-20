595e272c5880338ab535e10d406a626b0ffb327d
admc00 <admc0000@gmail.com>
2024-11-20 18:21:45 +0100

    Punto y Arista hechos

--- src/main/java/org/example/Arista.java
+++ src/main/java/org/example/Arista.java
@@ -1,4 +1,37 @@
 package org.example;
 
 public class Arista {
+    private final Punto x1;
+    private final Punto x2;
+    private double peso;
+
+    public Arista(Punto x1, Punto x2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.peso();
+    }
+
+    public Arista() {
+        this.x1 = new Punto();
+        this.x2 = new Punto();
+    }
+
+    public double getPeso() {
+        return this.peso;
+    }
+
+    private void peso() {
+        double pesoX = x1.getX() - x2.getX();
+        double pesoY = x1.getY() - x2.getY();
+        peso = Math.sqrt(Math.pow(pesoX, 2) + Math.pow(pesoY, 2));
+    }
+
+    public Punto getX1() {
+        return x1;
+    }
+
+    public Punto getX2() {
+        return x2;
+    }
+    
 }
--- src/main/java/org/example/Punto.java
+++ src/main/java/org/example/Punto.java
@@ -1,4 +1,44 @@
 package org.example;
 
 public class Punto {
+
+    private double x;
+    private double y;
+    private int id;
+
+    public Punto(double x, double y, int id) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+    public Punto() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.id = 0;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getID() {
+        return (float) this.id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
 }
