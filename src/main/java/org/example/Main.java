a4d4e7c10fdd0879da2a9f0d27e454c323a00576
jdc99 <jesus.diaz876@alu.uhu.es>
2024-12-03 20:27:27 +0100

    Algoritmo BusquedaVorazExhaustivaBi completado y correcion agregarCamino en Grafo.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
-import javax.swing.*;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Auxiliares.Menu();
+        Ficheros fichero = new Ficheros();
+        Grafo grafo = fichero.leerFichero("almonte5.tsp");
+        //System.out.println(grafo.toString());
+        System.out.println(BusquedaVorazExhaustivaBi.costeMinimo(grafo));
     }
 }
